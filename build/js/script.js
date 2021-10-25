9d82c5e1c4e504c6cf91224b107cd11504ff00a7
Nina Chernobrovkina <ninel_klem@mail.ru>
2021-10-25 23:41:16 +0300

    Шапка сайта

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,10 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+const pageHeader = document.querySelector('.header__nav');
+const headerToggle = document.querySelector('.header__toggle');
+const headerText = document.querySelector('.header__text');
 
-pageHeader.classList.remove('page-header--nojs');
+pageHeader.classList.remove('header__nav--nojs');
+headerText.classList.remove('header__text--nojs');
 
 headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+  pageHeader.classList.toggle('header__nav--open')
 });
